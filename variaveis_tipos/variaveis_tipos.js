18207c025a8e4963221223b18f51b08e88480052
jpmacleure <macleure-@hotmail.com>
2025-02-25 10:44:21 -0300

    Add variaveis_tipos

---
+++ variaveis_tipos/variaveis_tipos.js
@@ -0,0 +1,36 @@
+const n = 3;
+//n = 5;
+
+var j = 2;
+var j = 3;
+
+//console.log('j='+j);
+
+let i = 4;
+i = 6;
+
+//console.log('i='+i);
+
+let nome = "Joao";
+let cpf = '123';
+let telefone = '123';
+
+let tString = `Meu nome é ${nome}. Meu cpf é ${cpf}. Telefone: ${telefone}`;
+
+//console.log(tString);
+
+let x = 5.0;
+x = .5;
+x = 3;
+
+//console.log(typeof x);
+//console.log(typeof tString);
+
+let verdadeiro = true;
+let falso = false;
+
+let conexao = null;
+let conexao2;
+
+let y = x - x;
+console.log('y='+y);
