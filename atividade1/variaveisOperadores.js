a6d58b25c32d1c9606039fd3faf68e3eb8c8b653
jpmacleure <macleure-@hotmail.com>
2025-03-11 09:51:05 -0300

    add atividade1

--- atividade1/variaveisOperadores.js
+++ atividade1/variaveisOperadores.js
@@ -1,1 +1,15 @@
-a
+let num_A = 2;
+let num_B = 3.0;
+let num_C;
+
+num_C = num_A + num_B;
+console.log(`Resultado da adição entre ${num_A} e ${num_B} é ${num_C}`);
+
+num_C = num_A - num_B;
+console.log(`Resultado da subtração entre ${num_A} e ${num_B} é ${num_C}`);
+
+num_C = num_A * num_B;
+console.log(`Resultado da multiplicação entre ${num_A} e ${num_B} é ${num_C}`);
+
+num_C = num_A / num_B;
+console.log(`Resultado da divisão entre ${num_A} e ${num_B} é ${num_C}`);
