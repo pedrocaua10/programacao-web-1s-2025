01257e6f001262fae03c75927747616e4257c7cd
jpmacleure <macleure-@hotmail.com>
2025-03-14 09:24:51 -0300

    add atividade 2

--- atividade2/verificarNumeroPrimo.js
+++ atividade2/verificarNumeroPrimo.js
@@ -1,1 +1,28 @@
-a
+function verificarNumeroPrimo(n){
+    if(n <= 1){
+        return false;
+    }
+
+    if(n % 2 == 0 && n != 2){
+        return false;
+    }
+
+    for(let i = 3; i < n; i += 2){
+        if(n % i == 0){
+            return false;
+        }
+    }
+
+    return true;
+}
+
+console.log(`verificarNumeroPrimo(0)=${verificarNumeroPrimo(0)}`); //false
+console.log(`verificarNumeroPrimo(1)=${verificarNumeroPrimo(1)}`); //false
+console.log(`verificarNumeroPrimo(2)=${verificarNumeroPrimo(2)}`); //true
+console.log(`verificarNumeroPrimo(3)=${verificarNumeroPrimo(3)}`); //true
+console.log(`verificarNumeroPrimo(7)=${verificarNumeroPrimo(7)}`); //true
+console.log(`verificarNumeroPrimo(83)=${verificarNumeroPrimo(83)}`); //true
+console.log(`verificarNumeroPrimo(100)=${verificarNumeroPrimo(100)}`); //false
+console.log(`verificarNumeroPrimo(991)=${verificarNumeroPrimo(991)}`); //true
+console.log(`verificarNumeroPrimo(104729)=${verificarNumeroPrimo(104729)}`); //true
+console.log(`verificarNumeroPrimo(14348907)=${verificarNumeroPrimo(14348907)}`); //false
