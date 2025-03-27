18267ba4d52f998da1da20ba26034a66951f9e59
jpmacleure <macleure-@hotmail.com>
2025-03-27 09:26:33 -0300

    add app-calculadora

--- app_calculadora/calculadora.js
+++ app_calculadora/calculadora.js
@@ -1,16 +1,16 @@
-function somar(a, b){
+function somar(a, b) {
     return a + b;
 }
 
-function subtrair(a, b){
+function subtrair(a, b) {
     return a - b;
 }
 
-function multiplicar(a, b){
+function multiplicar(a, b) {
     return a * b;
 }
 
-function dividir(a, b){
+function dividir(a, b) {
     return a / b;
 }
 
--- app_calculadora/index.js
+++ app_calculadora/index.js
@@ -1,14 +1,44 @@
+// Para navegar até a pasta da aplicação: cd app_calculadora
+// Para criar o app npm: npm init
+// Instalar o express: npm install express@4.18.2
 const calc = require('./calculadora');
 const express = require('express');
-
 const app = express();
 
 app.get('/', (req, res)=>{
-    res.send('Olá, mundo!');
+    let html = '<h1>app_calculadora</h1>';
+    html += '<h3>Rotas da aplicação:</h3>';
+    html += '<p>/somar/:a/:b</p>';
+    html += '<p>/subtrair/:a/:b</p>';
+    html += '<p>/multiplicar/:a/:b</p>';
+    html += '<p>/dividir/:a/:b</p>';
+    res.send(html);
 });
 
-app.get('/ola/:nome', (req, res)=>{
-    res.send(`Olá, ${req.params.nome}!`);
+// Declara a rota /somar/:a/:b
+// E especifica o seu comportamento
+app.get('/somar/:a/:b', (req, res)=>{
+    let a = Number(req.params.a);
+    let b = Number(req.params.b);
+    res.send(`${a} + ${b} = ${calc.somar(a, b)}`);
+});
+
+app.get('/subtrair/:a/:b', (req, res)=>{
+    let a = Number(req.params.a);
+    let b = Number(req.params.b);
+    res.send(`${a} - ${b} = ${calc.subtrair(a, b)}`);
+});
+
+app.get('/multiplicar/:a/:b', (req, res)=>{
+    let a = Number(req.params.a);
+    let b = Number(req.params.b);
+    res.send(`${a} * ${b} = ${calc.multiplicar(a, b)}`);
+});
+
+app.get('/dividir/:a/:b', (req, res)=>{
+    let a = Number(req.params.a);
+    let b = Number(req.params.b);
+    res.send(`${a} / ${b} = ${calc.dividir(a, b)}`);
 });
 
 const PORT = 8080;
