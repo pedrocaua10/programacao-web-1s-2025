cbc56f3bf4133b905372be5dc0131ef478675f20
jpmacleure <macleure-@hotmail.com>
2025-04-04 09:52:28 -0300

    add app_estoque

--- app_estoque/estoque.js
+++ app_estoque/estoque.js
@@ -1,12 +1,13 @@
-const itens = [];
+let itens = [];
 
 function adicionar(item){
-
-    if(validar_item_a_cadastrar(item)){
+    let item_valido = validar_item_a_cadastrar(item);
+    
+    if(item_valido){
         itens.push(item);
-        return true;
     }
-    return false;
+
+    return item_valido;
 }
 
 function listar(){
@@ -14,7 +15,20 @@ function listar(){
 }
 
 function editar(id, qtd){
-    return editar_item(id, qtd);
+    if(
+        !is_numerico(id) || 
+        !is_id_cadastrado(id) ||
+        !is_numerico(qtd) ||
+        qtd < 0
+    ){
+        return false;
+    }
+    itens.forEach(item_cadastrado => {
+        if(item_cadastrado.id == id){
+            item_cadastrado.qtd = qtd;
+        }
+    });
+    return true;
 }
 
 module.exports = {
@@ -31,59 +45,31 @@ function is_numerico(n){
 }
 
 function is_id_cadastrado(id){
-    for(let indice in itens) {
-        let item_cadastrado = itens[indice];
-        if(item_cadastrado.id == id){
-            return true;
+    let item_existente = false;
+    itens.forEach(item_cadastrado => {
+        if(id == item_cadastrado.id){
+            item_existente = true;
         }
-    }
-    return false;
+    });
+    return item_existente;
 }
 
 function validar_item_a_cadastrar(item){
     let item_valido = true;
-    // Se o id for NaN, nulo ou menor igual a 0
-    // Não deve ser cadastrado
     if(!is_numerico(item.id) || item.id <= 0){
         item_valido = false;
     }
 
-    // Se o id já existitir, não deve ser cadastrado
     if(is_id_cadastrado(item.id)){
-        item_valido = false;
+        item_valido = false; 
+    }
+
+    if(item.nome.length == 0){
+        item_valido = false; 
     }
 
     if(!is_numerico(item.qtd) || item.qtd < 0){
         item_valido = false;
     }
-
-    if(item.nome.length < 1){
-        item_valido = false;    
-    }
-
     return item_valido;
-}
-
-function editar_item(id, qtd){
-    console.log(`${id} - ${qtd}`);
-    if(!is_numerico(id) || id == 0){
-        console.log('id invalido');
-        return false;
-    }
-    if(!is_id_cadastrado(id)){
-        console.log('id n cadastrado');
-        return false;
-    }
-    if(!is_numerico(qtd) || qtd < 0){
-        console.log('qtd invalido');
-        return false;
-    }
-
-    for(let indice in itens){
-        let item_cadastrado = itens[indice];
-        if(item_cadastrado.id == id){
-            item_cadastrado.qtd = qtd;
-        }
-    }
-    return true;
 }
--- app_estoque/index.js
+++ app_estoque/index.js
@@ -3,47 +3,38 @@ const estoque = require('./estoque');
 const app     = express();
 
 app.get('/', (req, res)=>{
-    let html = '<h1>app_estoque</h1>';
-    html += '<h3>Rotas disponíveis</h3>';
-    html += '<p>/adicionar/:id/:nome/:qtd - Adiciona um novo produto ao estoque</p>';
-    html += '<p>/listar - Lista todos os produtos do estoque</p>';
-    html += '<p>/remover/:id - Remove um produto do estoque</p>';
-    html += '<p>/editar/:id/:qtd - Altera a quantidade de um produto do estoque</p>';
-
+    let html =  '<h1>app_estoque</h1>';
+    html     += '<h3>Rotas disponíveis:</h3>'
+    html     += '<p>/adicionar/:id/:nome/:qtd</p>'
+    html     += '<p>/listar</p>'
+    html     += '<p>/remover/:id</p>'
+    html     += '<p>/editar/:id/:qtd</p>'
     res.send(html);
 });
 
-// /adicionar/:id/:nome/:qtd
 app.get('/adicionar/:id/:nome/:qtd', (req, res)=>{
-    const id   = Number(req.params.id);
-    const nome = req.params.nome;
-    const qtd  = Number(req.params.qtd);
-
-    const item = {
-        id: id,
-        nome: nome,
-        qtd: qtd
-    }
+    let item = {
+        id: Number(req.params.id),
+        nome: req.params.nome,
+        qtd: Number(req.params.qtd)
+    };
 
     res.send(estoque.adicionar(item));
 });
 
-// /listar
 app.get('/listar', (req, res)=>{
-   res.send(estoque.listar());
+    res.send(estoque.listar());
 });
 
-// /remover/:id
 app.get('/remover/:id', (req, res)=>{
-
+    
 });
 
-// /editar/:id/:qtd
 app.get('/editar/:id/:qtd', (req, res)=>{
-   const id  = Number(req.params.id);
-   const qtd = Number(req.params.qtd);
-   
-   res.send(estoque.editar(id, qtd));
+    let id  = Number(req.params.id);
+    let qtd = Number(req.params.qtd);
+
+    res.send(estoque.editar(id, qtd));
 });
 
 const PORT = 8080;
