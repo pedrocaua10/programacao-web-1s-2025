e656cb9f56548e5520cbd8c4bd7d4f94f7cf833b
jpmacleure <macleure-@hotmail.com>
2025-05-20 10:26:04 -0300

    add controllers

--- app_agendamento_consulta/index.js
+++ app_agendamento_consulta/index.js
@@ -7,31 +7,8 @@ app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
 app.use(express.urlencoded({extended: true}));
 
-app.get('/', (req, res)=>{
-    res.render('index.html');
-});
-
-app.post('/agendar_consulta', (req, res)=>{
-    let form_invalido = false;
-    let campos_invalidos = [];
-
-    let dados_consulta = req.body;
-
-    if(dados_consulta.nome.length == 0){
-        form_invalido = true;
-        campos_invalidos.push("Nome");
-    }
-    if(dados_consulta.sobrenome.length == 0){
-        form_invalido = true;
-        campos_invalidos.push("Sobrenome");
-    }
-    if(dados_consulta.cpf.length == 0){
-        form_invalido = true;
-        campos_invalidos.push("CPF");
-    }
-
-    res.render('index.html', {form_invalido, campos_invalidos, dados_consulta});
-});
+const agendamentoRouter = require('./routers/agendamentoRouter');
+app.use('/', agendamentoRouter);
 
 const PORT = 8080;
 app.listen(PORT, ()=>{
