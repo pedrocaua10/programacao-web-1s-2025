b4478691294f7052c21f985395593b093aeb045d
jpmacleure <macleure-@hotmail.com>
2025-06-03 10:28:50 -0300

    add model

--- app_agendamento_consulta/index.js
+++ app_agendamento_consulta/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mustacheExpress = require('mustache-express');
+const db  = require('./db');
 const app = express();
 
 app.engine('html', mustacheExpress());
@@ -9,6 +10,8 @@ app.use(express.urlencoded({extended: true}));
 
 const agendamentoRouter = require('./routers/agendamentoRouter');
 app.use('/', agendamentoRouter);
+
+db.sync();
 
 const PORT = 8080;
 app.listen(PORT, ()=>{
